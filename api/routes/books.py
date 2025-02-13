f820eca5a4c6015fc646a256dc7eb587aee47790
Awajis <ibotyle@gmail.com>
2025-02-13 01:38:43 +0100

    fix import for HTTPException

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,7 +1,7 @@
-from http.client import HTTPException
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
+
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
